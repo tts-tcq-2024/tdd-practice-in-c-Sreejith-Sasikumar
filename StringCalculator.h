2aefa7d15f668084bfca256f275e241468959cb0
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-08 17:13:52 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,9 +2,9 @@
 
 bool isEmptyString(const char* input){
   if (input == NULL || input[0] == '\0') {
-    return TRUE; // The string is empty
+    return 1; // The string is empty
   }
-  return FALSE;
+  return 0;
 }
 
 int add(const char *input){
