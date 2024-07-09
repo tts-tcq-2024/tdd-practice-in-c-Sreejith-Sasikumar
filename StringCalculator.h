43eedf8a0067c90fdad148d396473f5d82ac5aae
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-09 16:22:25 +0530

    Update StringCalculator.h - fetchNumbers refactored

--- StringCalculator.h
+++ StringCalculator.h
@@ -3,16 +3,21 @@
 
 #include "StringCalculator.h"
 #include <ctype.h>
-int nums[]={0,0};
+int nums[]={0,0}, num = 0;
+
+int digitsSum(const char* str, int i){
+	while(isdigit(str[i]))
+		num = num*10+(str[i++]-'0');
+	return i;
+}
 
 void fetchNumbers(const char* str){
 	int i=0,j=0, sum=0;
 	while(str[i]){
 		if(isdigit(str[i])){
-			while(isdigit(str[i]))
-				sum = sum*10+(str[i++]-'0');
-			nums[j++] = sum;
-			sum = 0;
+			i = digitsSum(str, i);
+			nums[j++] = num;
+			num = 0;
 			i--;
 		}
 		i++;
