adf292d1d7855a2776392f5564c04de0edbfa633
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-09 23:07:23 +0530

    Update StringCalculator.Tests.cpp - 2num testcase alone

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "StringCalculator.h"
-
+/*
 TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
     int expectedresult = 0;
     const char* input = "";
@@ -14,14 +14,14 @@ TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-/*
+*/
 TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int expectedresult = 3;
     const char*  input = "1,2";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-
+/*
 TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
     const char*  input = "1\n2,3";
