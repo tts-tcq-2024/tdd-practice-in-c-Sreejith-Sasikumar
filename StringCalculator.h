f5ed20e03f341f2a99138b5f127234f32865c696
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-09 14:59:15 +0530

    Update StringCalculator.h - todo code uncommented

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,26 +4,14 @@
 #include "StringCalculator.h"
 #include <ctype.h>
 
-int add(const char *input){
-  int sum = 0, i=0;
-  while(input[i]){
-    if(isdigit(input[i])){
-      sum = sum + (input[i] - '0');
-    }
-    i++;
-  }
-  return sum;
-}
-/* TO-DO
 int* fetchNumbers(char* str){
-	int nums[]={0,0};
-	int i=0,j=0, sum=0;
-	while(str[i])
+int nums[]={0,0};
+int i=0,j=0, sum=0;
+	while(str[i]){
 		if(isdigit(str[i])){
 			do{
 				sum = sum*10+(str[i++]-'0');
 			}while(isdigit(str[i]));
-			
 			nums[j++] = sum;
 			sum = 0;
 			i--;
@@ -31,7 +19,32 @@ int* fetchNumbers(char* str){
 		i++;
 	}
 }
-*/
+
+int digitsSum(int a, int b){
+	int sum = 0;
+	while(a){
+		sum = sum + a%10;
+		a/=10;
+	}
+	while(b){
+		sum = sum + b%10;
+		b/=10;
+	}
+	return sum;
+}
+
+int overThousand(int a, int b){
+	a = a>1000?0:a;
+	b = b>1000?0:b;
+	return digitsSum(a,b);
+}
+
+int add(const char *input){
+	int nums[2] = fetchNumbers(input);
+	int sum = overThousand(nums[0], nums[1]);
+	return sum;
+}
+
 
 
 #endif // STRINGCALCULATOR_H
