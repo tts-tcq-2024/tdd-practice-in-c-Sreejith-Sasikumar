2c8c572b0df386d3c6607eae75be587ad9b4a08b
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-09 22:45:01 +0530

    Update StringCalculator.h - remove global sum variable

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,25 +4,27 @@
 #include "StringCalculator.h"
 #include <ctype.h>
 
-int digitSum = 0;
-void sumOfDigits(int s){
-	if (s<=1000)
+int sumOfDigits(int s){
+	int sum = 0;
+	if (s<=1000){
 		while(s){
-			digitSum = digitSum + s%10;
+			sum = sum + (s%10);
 			s/=10;
 		}
+	}
+	return sum;
 }
 
 int add(const char *str){
-	int i=0, s=0;
+	int i=0, s=0, sum = 0;
 	while(str[i]){
 		while(isdigit(str[i++])){
-			s = s*10 + str[i-1]-'0';
+			s = (s*10) + str[i-1]-'0';
 		}
-		//sumOfDigits(s);
+		sum = sum + sumOfDigits(s);
 		s=0;
 	}
-	return digitSum;
+	return sum;
 }
 
 #endif // STRINGCALCULATOR_H
