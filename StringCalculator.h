dce8c8505ad1f2829730cb3831d77571d2ba8e05
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-11 23:54:18 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -32,7 +32,8 @@ int fetchNumber(const char* str, int i, int* s){
 
 int add(const char *str){
 	int i=0, s=0, sum = 0;
-	if (!isNegative(str))
+	bool neg = isNegative(Str);
+	if (neg==false)
 		while(str[i]){
 			i = fetchNumber(str, i, &s);
 			sum = sum + sumOfDigits(s);
