63cfa3bf675d39221c92ccbf20f4de203d1b8009
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-15 10:48:27 +0530

    Update StringCalculator.h - given comments

--- StringCalculator.h
+++ StringCalculator.h
@@ -22,11 +22,20 @@ int sumOfDigits(int s){
 	}
 	return sum;
 }
-
-int fetchNumber(const char* str, int i, int* s){
-	while(str[i]&&isdigit(str[i++])){
-		*s = ((*s)*10) + str[i-1]-'0';
-	}
+/* 
+Function: fetchNumberFromString
+	Used to fetch the numbers (in integer format) from the input string
+Parameters: 
+	str: 	Input string (read-only), 
+ 	i: 	Index position in the string, 
+  	number: To fetch the number from input string in integer format (called by reference)
+Return value:
+	Returns the position in the string where it lastly found a digit
+*/
+int fetchNumberFromString(const char* str, int i, int* number){	
+	while(str[i]&&isdigit(str[i++])){			// Check whether each character is a digit or not
+		*number = ((*number)*10) + str[i-1]-'0';	// Actual number (single digit or multiple digits) is being calculated in integer format, from the character formats
+	}							
 	return i;
 }
 
@@ -35,7 +44,7 @@ int add(const char *str){
 	bool neg = isNegative(str);
 	if (neg==false)
 		while(str[i]){
-			i = fetchNumber(str, i, &s);
+			i = fetchNumberFromString(str, i, &s);
 			sum = sum + sumOfDigits(s);
 			s=0;
 		}
